e26e478baebba9f8bd6ca27497393d0b44ffb119
Christian Rojas <44682470+BlueFits@users.noreply.github.com>
2022-08-16 16:03:14 -0400

    Init commit

--- src/constants/commands.ts
+++ src/constants/commands.ts
@@ -1,4 +1,5 @@
 export default {
     DOM_LAUNCH: "dom-js-inject.launch",
     GEN_PERS_TEMPLATE: "dom-js-inject.gen_pers_template",
+    GEN_CDB_FILES: "dom-js-inject.gen_cdb_files",
 };
--- src/extension.ts
+++ src/extension.ts
@@ -8,19 +8,32 @@ import templates from "./constants/templates";
 import { menu, ES6_TEMPLATE, VANILLA_TEMPLATE, PERS_COOKIE_TEMPLATE } from "./constants/menu";
 import { readFilePath } from "./utils";
 
-const { DOM_LAUNCH, GEN_PERS_TEMPLATE } = commands;
+const { DOM_LAUNCH, GEN_PERS_TEMPLATE, GEN_CDB_FILES } = commands;
 const { es6TemplateGen, vanillaTemplateGen, persCookieTemplateGen } = templates;
 
 export async function activate(context: vscode.ExtensionContext) {
 
 	let browserInstance: any = null;
 
+	//Methods
 	const reloadAction = async (e: any) => {
 		if (vscode.window.activeTextEditor?.document.uri.fsPath === browserInstance.getFilePath()) {
 			await browserInstance.reloadTab();
 			await browserInstance.render();
 		}
 	};
+
+	//Commands
+	context.subscriptions.push(vscode.commands.registerCommand(GEN_CDB_FILES, async () => {
+		vscode.window.showInformationMessage("salutations my friend");
+
+		
+		// let url = await vscode.window.showInputBox({prompt: 'Url', placeHolder: 'Url'});
+		// if (!url) {throw new Error("cancelled");} else if (!Validator.isURL(url)) {throw new Error("Not a valid url");};
+		// const onSaveCleaner = vscode.workspace.onDidSaveTextDocument(reloadAction);
+		// browserInstance = await PuppeteerBrowser.build(url, { onSaveCleaner });
+		// await browserInstance.start();
+	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand(DOM_LAUNCH, async () => {
 		let url = await vscode.window.showInputBox({prompt: 'Url', placeHolder: 'Url'});
