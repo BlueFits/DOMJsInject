e28346f2c16d59ea08dc9fd042ea69d2832056f6
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-10-06 09:53:00 -0400

    removed tavc

--- src/constants/templates/es6.ts
+++ src/constants/templates/es6.ts
@@ -9,24 +9,6 @@ export default `
     window.persDOM_INJECT_SCRIPTChangesApplied = false;
 
     const persDOM_INJECT_SCRIPTChangesToApply = async () => {
-        /* Used for TAVC */
-        const orig_$ = $;
-        $ = (param, options = { isImportant: false }) => {
-            $.selectors = $.important && $.important.length > 0 ? [...$.important] : [];
-            $.important = $.important && $.important.length > 0 ? [...$.important] : [];
-            $.selectors.push({
-                selector: param,
-                length: orig_$(param).length,
-            });
-            if (options.isImportant) {
-                $.important.push({
-                    selector: param,
-                    length: orig_$(param).length,
-                });
-            }
-            return orig_$(param);
-        }
-        
         const tryCatch = (fun, block) => {
             try {fun();} catch (err) {console.trace('%c ' + 'PERS-DOM_INJECT_SCRIPT' + ' error in ' + block + ': ' + err, 'background: #222; color: #AD7150');}
         }
